cc8795d1cfc6eaf2896db258cc03d8c663490c53
Arthur Santiago F. <arthursantiagofrrz@gmail.com>
2026-09-24 12:42:31 -0300

    Refactor initRouter function to improve readability

--- js/router.js
+++ js/router.js
@@ -1,27 +1,19 @@
-// Responsabilidade de gerenciar as rotas da aplicação e ativar links no menu.
-
 export function initRouter(onRouteChange) {
   document
     .querySelectorAll('#main-nav [data-route]')
     .forEach(link => {
-
       link.addEventListener('click', event => {
         event.preventDefault();
-
         const route = link.getAttribute('data-route');
-
         document
           .querySelectorAll('#main-nav .nav-link')
           .forEach(navLink => {
             navLink.classList.remove('active');
             navLink.removeAttribute('aria-current');
           });
-
         link.classList.add('active');
         link.setAttribute('aria-current', 'page');
-
         onRouteChange(route);
       });
-
     });
 }
